22bdc69b95a6d2c195e9f8d6b5bc9936d52fb15c
Richa <richa@Ranus-MacBook-Pro.local>
2020-01-10 20:39:21 +0530

    initial commit

---
+++ writetocsv.py
@@ -0,0 +1,21 @@
+from skimage.io import imread, imshow
+from skimage.transform import resize
+from skimage.feature import hog
+from skimage import exposure
+import matplotlib.pyplot as plt
+import numpy
+import sys
+import csv
+import pandas as pd
+numpy.set_printoptions(threshold=sys.maxsize)
+
+data = pd.read_csv("data.csv")
+list1 = []
+for i in range(1,69):
+    list1.append('good')
+for i in range(1,78):
+    list1.append('ugly')
+data.insert(loc=3780, column='good', value=list1)
+# data.drop(data.columns[0], axis=1)
+data.to_csv('./data.csv')
+print(data)
