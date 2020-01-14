eca42904c8ca2db11ce0e969127863f93cd1a790
Arpit Agarwal <arpit@arpits-mbp.lan>
2020-01-14 00:16:53 -0800

    resized good carrots

--- resize.py
+++ resize.py
@@ -1,16 +1,16 @@
 from PIL import Image
 import os, sys
 
-path = ("./dataset/GoodCarrotsCropped/")
+path = ("./dataset/GoodCarrotsCropped/finalGoodRotated/")
 
 def resize():
     for item in os.listdir(path):
         if os.path.isfile(path+item):
-            script_dir = os.path.dirname(os.path.abspath("./dataset/GoodCarrotsCropped/"+__file__))
+            script_dir = os.path.dirname(os.path.abspath("./dataset/GoodCarrotsCropped/finalGoodRotated/"+__file__))
             im = Image.open(os.path.join(script_dir, item))
             im = im.convert("RGBA")
             f, e = os.path.splitext(item)
             imResized = im.resize((640,480), Image.ANTIALIAS)
-            imResized.save(f + 'Resized.png', 'PNG', quality=90)
+            imResized.save(f + 's.png', 'PNG', quality=90)
 
 resize()
