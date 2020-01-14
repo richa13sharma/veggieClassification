e9c37ae1c9df42e20e2b817e7a825406ee1c15b3
Arpit Agarwal <arpit@arpits-mbp.lan>
2020-01-14 01:20:49 -0800

    hog descriptors ready

--- hogDescriptor.py
+++ hogDescriptor.py
@@ -7,21 +7,30 @@ import matplotlib.pyplot as plt
 import numpy
 import sys
 import csv
+import os
+from os import path
 numpy.set_printoptions(threshold=sys.maxsize)
-for i in range(1,100):
-    img = imread('./dataset/UglyCarrots/resized/ruc{0}.jpeg'.format(i)) 
-    resized_img = resize(img, (128,64))
 
-    fd, hog_image = hog(resized_img, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=True, multichannel=True) # orientations = number of bins for histogram
 
-    fdlist = fd.tolist() 
- 
-    hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 10))
+for i in range(1,342):
+    path1 = './dataset/GoodCarrotsCropped/finalGoodRotated/gr{0}s.png'.format(i)
+    if (path.exists(path1)):
+        img = imread('./dataset/GoodCarrotsCropped/finalGoodRotated/gr{0}s.png'.format(i))
+    
+        resized_img = resize(img, (128,64))
+        
+        fd, hog_image = hog(resized_img, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2), visualize=True, multichannel=True) # orientations = number of bins for histogram
+        
+        fdlist = fd.tolist() 
+    
+        hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 10))
 
-    res = fdlist
-    csvfile = './richa.csv'
+        res = fdlist
+        csvfile = './goodAfterHog.csv'
 
-    with open(csvfile, mode='a') as output:
-        writer = csv.writer(output)
-        writer.writerow(res)
-        # writer.writerow([])
+        with open(csvfile, mode='a') as output:
+            writer = csv.writer(output)
+            writer.writerow(res)
+            # writer.writerow([])
+    else:
+        continue
