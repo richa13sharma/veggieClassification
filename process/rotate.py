d4894db4c630db985547891c06afbf9dc445d842
Richa <richa@Ranus-MacBook-Pro.local>
2020-01-17 23:14:34 +0530

    only Good rotated

--- process/rotate.py
+++ process/rotate.py
@@ -1,26 +1,26 @@
 from PIL import Image
 import os, sys
 
-# path = ("./dataset/GoodCarrotsCropped/")
-path = ("./dataset/UglyCarrots/")
+#path = ("../dataset/GoodCarrotsCropped/")
+path = ("../dataset/UglyCarrots/")
 
 def rotate():
     for item in os.listdir(path):
         if os.path.isfile(path+item):
             if item == path + '.DS_Store':
                 continue
-            script_dir = os.path.dirname(os.path.abspath("./dataset//UglyCarrots/"+__file__))
+            #script_dir = os.path.dirname(os.path.abspath("../dataset/GoodCarrotsCropped/"+__file__))
+            script_dir = os.path.dirname(os.path.abspath("../dataset/UglyCarrots/"+__file__))
             im = Image.open(os.path.join(script_dir, item)) #create image object
             file, ext = os.path.splitext(item)
-            angle90 = 90
             angle180 = 180
-            angle45 = 45
-            # imRotate90 = im.rotate(angle90 , expand = 'true' , fillcolor = 'white')
-            # imRotate90.save(file + 'Rotated90.png', 'PNG', quality=90)
-            imRotate45 = im.rotate(angle45 , expand = 'true' , fillcolor = 'white')
-            imRotate45.save(file + 'Rotated45.png', 'PNG', quality=90)
             # imRotate180 = im.rotate(angle180 , expand = 'true' , fillcolor = 'white')
             # imRotate180.save(file + 'Rotated180.png', 'PNG', quality=90)
+            for degree in range(30,361,30):
+                imRotate= im.rotate(degree , expand = 'true' , fillcolor = 'white')
+                i = str(degree)
+                imRotate.save(file + 'URotated'+i+'.png', 'PNG', quality=90)
+                #print("saved")
             
         
 rotate()
