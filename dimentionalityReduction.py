46b0876f7471339e41a4103cb27c51bac32dad95
arpit <arpit68agarwal@gmail.com>
2020-06-18 16:37:35 +0530

    PCA on new dataset

--- dimentionalityReduction.py
+++ dimentionalityReduction.py
@@ -4,18 +4,18 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
-df = pd.read_csv("dataCopy.csv")
+df = pd.read_csv("data.csv")
 print(df.head())
 
 features = df.iloc[:, -2]
-x = df.iloc[:, features]
-y = df.loc[:,['1']].values
+x = df.iloc[:, 0:-1]
+y = df.iloc[:,-1]
 x = StandardScaler().fit_transform(x)
 
-pca = PCA(n_components=1522)
+pca = PCA(n_components=3208)
 principalComponents = pca.fit_transform(x)
 principalDf = pd.DataFrame(data = principalComponents)
-finalDf = pd.concat([principalDf, df[['1']]], axis = 1)
+finalDf = pd.concat([principalDf, df.iloc[:,-1]], axis = 1)
 
 print(finalDf)
 finalDf.to_csv("reducedData.csv")
